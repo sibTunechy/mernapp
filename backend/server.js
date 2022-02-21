b44b86869541c6022d4007c83e3475953512b8ad
sibTunechy <ayotemitope930@gmail.com>
2022-02-21 20:42:14 +0100

    more server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log('Hello Africa')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5050;
+
+const app = express()
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
