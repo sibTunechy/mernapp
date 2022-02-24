13e3448f8c6876ea5c8d90d59a08c9a31e30abea
sibTunechy <ayotemitope930@gmail.com>
2022-02-24 21:32:33 +0100

    more mongoose

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,9 +1,13 @@
 const asyncHandler = require('express-async-handler');
+
+const Goal = require('../models/goalModel')
 // @desc Get goals 
 // @route GET /api/goals
 // @access private
-const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get goals'})
+const getGoals = asyncHandler(async (req, res) => {          // getGoals is a function
+    const goals = await Goal.find()                                            // create a variable called goals
+
+    res.status(200).json(goals)
 })
 
 // @desc Create goals 
@@ -15,21 +19,46 @@ const createGoals = asyncHandler( async (req, res) => {
         throw new Error('Please add a text field')
     }
 
-    res.status(200).json({message: 'Create goals'})
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json(goal)
 })
 
 // @desc Update goals 
 // @route PUT /api/goals/:id
 // @access private
 const updateGoals = asyncHandler( async (req, res) => {
-    res.status(200).json({message: `Update goals ${req.params.id}`})
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {                           // if goal is not found... throw error 
+        res.status(400)
+        throw new Error('Goal not Found')
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+
+    res.status(200).json(updatedGoal)
 })
 
 // @desc Delete goals 
 // @route DELETE /api/goals/:id
 // @access private
 const deleteGoals = asyncHandler( async (req, res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
+
+    goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+    
+    await goal.remove()
+
+    res.status(200).json({id: req.params.id})
 })
 
 
